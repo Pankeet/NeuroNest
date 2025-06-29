a2d9bb265277639030d74b16e60a8d339879e7fe
Pankeet <pankeet04@gmail.com>
2025-06-29 10:33:07 +0530

    Initialized Project

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,67 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
 import mongoose  from 'mongoose';
+import bcrypt from 'bcrypt';
+import { UserModel } from './db';
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
+app.use(express.json());
 
-app.post('api/v1/signup' , (req, res) => {
+const salt = 9;
+const JWT_SECRET = "NeuroNest1029384756"
 
+app.post('/api/v1/signup' , async (req , res): Promise<void> => {
+    // user.phone
+    const user = req.body;
+    try{
+        const UserExsists = await UserModel.findOne({username : user.username});
+        if(UserExsists){
+            res.status(411).json({
+                message : "User Already Exsists"
+            })
+            return ;
+        }
+        const hashPassword = await bcrypt.hash(user.password , salt);
+        await UserModel.create({
+            user: user.username,
+            password : hashPassword,
+            name: user.name,
+            age: user.age,
+            phoneNumber: user.phone
+        })
+
+        res.status(200).json({
+            message : "SignUp Successful"
+        })
+        return;
+    }
+    catch(err){
+        console.log(err);
+    }
 });
 
-app.post('api/v1/signin' , (req , res) => {
+app.post('api/v1/signin' , async (req, res) => {
 
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const user = await UserModel.findOne({
+        username :username,
+        password : password
+    });
+
+    if(user) {
+        const token = jwt.sign({
+            id: user._id,
+        } , JWT_SECRET);
+
+        res.status(200).json({
+            token  : token,
+            message : " Sign in Successful"
+        })
+        return;
+    }
 });
 
 app.post('/api/v1/content' , (req,res) => {
@@ -30,3 +83,10 @@ app.post('api/v1/brain/share' , (req, res) => {
 app.get('api/v1/brain/:shareLink' , (req, res) => {
 
 });
+
+
+
+   
+   
+
+ 
