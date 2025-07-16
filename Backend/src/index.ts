61615daf9e8eec8df765618f3cbe23a65d74cb48
Pankeet <pankeet04@gmail.com>
2025-07-16 18:05:33 +0530

    Version 1.1.3 D

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -13,7 +13,7 @@ app.use('/api/v1' , userRouter);
 async function startServer(){
     try {
         await connectDb(); 
-        const PORT = process.env.PORT || 3001
+        const PORT = process.env.PORT || 3001;
         app.listen(PORT , () => {
             console.log('Server is running on port 3001');
         });
